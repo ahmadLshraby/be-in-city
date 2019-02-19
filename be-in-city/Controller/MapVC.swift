ebfce1b4eca5ae60613fcc516ebe7f403175aeac
Ahmad Shraby <ahmadelshraby10@outlook.com>
2019-02-19 07:20:01 +0200

    UI

--- be-in-city/Controller/MapVC.swift
+++ be-in-city/Controller/MapVC.swift
@@ -7,24 +7,24 @@
 //
 
 import UIKit
+import MapKit
 
 class MapVC: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+    }
+    
+    @IBAction func centerMapBtn(_ sender: UIButton) {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+}
 
+
+extension MapVC: MKMapViewDelegate {
+    
 }
