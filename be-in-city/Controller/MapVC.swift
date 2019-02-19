926110c083e0863cdcbcefa4c2cfdde61a390fbd
Ahmad Shraby <ahmadelshraby10@outlook.com>
2019-02-19 06:38:20 +0200

    Start
    Build folders MVC

---
+++ be-in-city/Controller/MapVC.swift
@@ -0,0 +1,30 @@
+//
+//  MapVC.swift
+//  be-in-city
+//
+//  Created by sHiKoOo on 2/19/19.
+//  Copyright © 2019 sHiKoOo. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
